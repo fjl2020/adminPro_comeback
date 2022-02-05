18e2b3c5cf3db3a0b435086c2224e10813b76971
fjl2020 <fjl2020@gmail.com>
2022-02-05 17:39:49 +0100

    routing implement

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import {  RouterModule, Routes} from "@angular/router";
+
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+
+const routes:Routes=[
+  {path:'dashboard',component:DashboardComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'progress',component:ProgressComponent},
+  {path:'grafica1',component:Grafica1Component},
+  {path:'',redirectTo:"/dashboard",pathMatch:"full"},
+  {path:'**',component:NopagesfoundComponent},
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[ RouterModule]
+})
+export class AppRoutingModule { }
+ 
